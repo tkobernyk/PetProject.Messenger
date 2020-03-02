558204d47daac0eb6ecd3195eeb37d28f868a1e2
Taras_Kobernyk <tara_kobernyk@epam.com>
2020-03-02 18:26:31 +0200

    Add project files.

---
+++ PetProject.Messenger.Web/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PetProject.Messenger.Web.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Ping()
+        {
+            return Ok("Hey!");
+        }
+    }
+}
---
+++ PetProject.Messenger.Web/Startup.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace PetProject.Messenger.Web
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc(options => { options.EnableEndpointRouting = false; });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseMvc();
+        }
+    }
+}
